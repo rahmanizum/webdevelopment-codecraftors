99323219b7469e6696e44b3fa13be2ba0fe3378c
Mufil Rahman A <mufil.r@simplicontract.com>
2025-02-17 10:28:45 +0530

    feat: add toggle functionality for submit button visibility with new click event

--- 03-dom-manipulation/04-class/main.js
+++ 03-dom-manipulation/04-class/main.js
@@ -1,16 +1,15 @@
-const btn = document.createElement('button');
-btn.className = 'btn btn-dark mt-2';
-btn.id = 'submi-btn';
-btn.setAttribute('title', 'Submit');
-const text = document.createTextNode('Submit');
-btn.appendChild(text);
+const btn = document.querySelector('#btn');
+const submitbtn = document.querySelector('input[type="submit"]');
+console.log(submitbtn)
 
-main_div = document.getElementById('main');
-main_div.appendChild(btn);
+btn.addEventListener('click',clickHandler);
 
-var button = document.getElementById('submi-btn');
-button.addEventListener('click',()=>{
-    alert('Button Clicked');
-});
-
-
+function clickHandler(e){
+    if(submitbtn.classList.contains('d-none')){
+        submitbtn.classList.remove('d-none');
+    }
+    else{
+        submitbtn.classList.add('d-none');
+    }
+    
+}
