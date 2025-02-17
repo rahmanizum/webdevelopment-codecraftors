649489f4514eee804cdded21d97f01a09a95cdd4
Mufil Rahman A <mufil.r@simplicontract.com>
2025-02-17 20:13:49 +0530

    feat: refactor event listeners for input and button interactions, add form submission handling

--- 03-dom-manipulation/05-class/main.js
+++ 03-dom-manipulation/05-class/main.js
@@ -1,22 +1,21 @@
-var input = document.querySelector('input[type="text"]');
-var button = document.querySelector('input[type="submit"]');
-items = document.querySelector('#items');
+const input = document.querySelector('input[type="text"]');
+const button = document.querySelector('input[type="submit"]');
+const  select = document.querySelector('select');
+const items = document.querySelector('#items');
+const form = document.querySelector('form');
 
 
 input.addEventListener('input', inputEvent);
-button.addEventListener('click', buttonEvent);
+select.addEventListener('change',changeEvent);
 
 
 function inputEvent(e){
     console.log(e.target.value);
 }
 
-function buttonEvent(e){
-    e.preventDefault();
-    const li = document.createElement('li');
-    li.classList.add('list-group-item');
-    li.textContent = input.value;
-    items.appendChild(li);
+function changeEvent(e){
+    console.log(`Option selected: ${e.target.value}`);
+
 }
 
 input.addEventListener('focus',runEVent);
@@ -31,9 +30,31 @@ function runEVent(e){
 }
 
 
-select = document.querySelector('select');
-select.addEventListener('change',changeEvent);
 
-function changeEvent(e){
-    console.log(`Option selected: ${e.target.value}`);
+
+
+button.addEventListener('click', buttonEvent);
+
+function buttonEvent(e){
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = input.value;
+    items.appendChild(li);
 }
+
+
+form.addEventListener('submit', formEvent);
+
+function formEvent(e){
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = input.value;
+    items.appendChild(li);
+}
+
+
+
+
+
