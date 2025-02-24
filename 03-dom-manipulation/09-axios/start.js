f72c79a69ae242c0097a752e5dec69bda17b9453
Mufil Rahman A <mufil.r@simplicontract.com>
2025-02-24 21:56:08 +0530

    refactor: streamline event listener declaration for transformResponse

--- 03-dom-manipulation/09-axios/start.js
+++ 03-dom-manipulation/09-axios/start.js
@@ -90,8 +90,6 @@ function getTodos() {
   document.getElementById('delete').addEventListener('click', removeTodo);
   document.getElementById('sim').addEventListener('click', getData);
   document.getElementById('headers').addEventListener('click', customHeaders);
-  document
-    .getElementById('transform')
-    .addEventListener('click', transformResponse);
+  document.getElementById('transform').addEventListener('click', transformResponse);
   document.getElementById('error').addEventListener('click', errorHandling);
   document.getElementById('cancel').addEventListener('click', cancelToken);
